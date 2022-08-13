6538e89ecfa9c3d739afbe334fbdc782c3e5e79f
pavlo.pavlenko <pavlo.pavlenko@tangramltd.com>
2022-08-13 12:46:39 +0300

    auth/register logic
    no seed item && attach. no testing

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,23 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+/**
+ *
+ */
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles {
+        assignRole as spatieAssignRole;
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +51,64 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return HasMany
+     */
+    public function shipping(): HasMany
+    {
+        return $this->hasMany(ShopOrder::class, 'user_id', 'id');
+    }
+
+    /**
+     * @return HasOne
+     */
+    public function profile(): HasOne
+    {
+        return $this->hasOne(UserProfile::class, 'user_id', 'id');
+    }
+
+    /**
+     * @return HasOne
+     */
+    public function social(): HasOne
+    {
+        return $this->hasOne(UserSocial::class, 'user_id', 'id');
+    }
+
+    /**
+     * @return HasManyThrough
+     */
+    public function addresses(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            UserAddress::class,
+            UserProfile::class,
+            'user_id',
+            'user_profile_id',
+            'id',
+            'id'
+        );
+    }
+
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
