69b48e66418edbbdd8549ca1d9c9a0441cda1f92
pavlo.pavlenko <pavlo.pavlenko@tangramltd.com>
2022-11-10 12:29:10 +0200

    somefix

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
-use App\Repository\Eloquent\UserProfileRepository;
+
 use App\Repository\Eloquent\UserRepository;
-use App\Repository\UserProfileRepositoryInterface;
+
 use App\Repository\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 use App\Repository\EloquentRepositoryInterface;
@@ -16,19 +16,6 @@ use App\Repository\Eloquent\BlogPostRepository;
 use App\Repository\Eloquent\BlogItemRepository;
 use App\Repository\Eloquent\BlogItemAttachmentRepository;
 
-use App\Repository\Eloquent\ShopOrderRepository;
-use App\Repository\Eloquent\ShopOrderProductRepository;
-use App\Repository\Eloquent\ShopProductRepository;
-use App\Repository\Eloquent\ShopDiscountRepository;
-use App\Repository\Eloquent\ShopCarrierRepository;
-use App\Repository\ShopOrderRepositoryInterface;
-use App\Repository\ShopOrderProductRepositoryInterface;
-use App\Repository\ShopProductRepositoryInterface;
-use App\Repository\ShopDiscountRepositoryInterface;
-use App\Repository\ShopCarrierRepositoryInterface;
-
-use App\Repository\Eloquent\UserAddressRepository;
-use App\Repository\UserAddressRepositoryInterface;
 
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -46,14 +33,6 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(BlogItemAttachmentRepositoryInterface::class, BlogItemAttachmentRepository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
 
-        $this->app->bind(ShopOrderRepositoryInterface::class, ShopOrderRepository::class);
-        $this->app->bind(ShopOrderProductRepositoryInterface::class, ShopOrderProductRepository::class);
-        $this->app->bind(ShopProductRepositoryInterface::class, ShopProductRepository::class);
-        $this->app->bind(ShopDiscountRepositoryInterface::class, ShopDiscountRepository::class);
-        $this->app->bind(ShopCarrierRepositoryInterface::class, ShopCarrierRepository::class);
-
-        $this->app->bind(UserAddressRepositoryInterface::class, UserAddressRepository::class);
-        $this->app->bind(UserProfileRepositoryInterface::class, UserProfileRepository::class);
     }
 
     /**
